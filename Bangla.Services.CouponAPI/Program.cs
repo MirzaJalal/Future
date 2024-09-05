f6448f025723c2d00ce9d20900dbcd4e2725cf37
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-09-05 11:16:59 +0200

    pending migration automation

--- Bangla.Services.CouponAPI/Program.cs
+++ Bangla.Services.CouponAPI/Program.cs
@@ -29,4 +29,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        ApplicationDbContext _db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0 )
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
