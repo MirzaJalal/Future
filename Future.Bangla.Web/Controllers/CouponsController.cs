02803e1d61e14e81c1ba787a21fc8b8adda363f1
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-09-08 12:47:35 +0200

    CouponIndex in the controller of webApp

---
+++ Future.Bangla.Web/Controllers/CouponsController.cs
@@ -0,0 +1,28 @@
+﻿using Future.Bangla.Web.Models;
+using Future.Bangla.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Future.Bangla.Web.Controllers
+{
+    public class CouponsController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponsController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto> list = new List<CouponDto>();
+
+            ResponseDto? response = await _couponService.GetAllCouponAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
