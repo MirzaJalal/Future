7de07ae62f43aa8ef3d4b9247fbf0634b29fef03
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-09-13 22:50:18 +0200

    Authentication Service implementation

---
+++ Bangla.Services.AuthenticationAPI/Service/AuthService.cs
@@ -0,0 +1,34 @@
+﻿using Bangla.Services.AuthenticationAPI.Data;
+using Bangla.Services.AuthenticationAPI.Models;
+using Bangla.Services.AuthenticationAPI.Models.Dto;
+using Bangla.Services.AuthenticationAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bangla.Services.AuthenticationAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(ApplicationDbContext context, 
+           UserManager<ApplicationUser> userManager,
+           RoleManager<IdentityRole> roleManager
+           )
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bangla.Services.AuthenticationAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Bangla.Services.AuthenticationAPI.Models.Dto;
+
+namespace Bangla.Services.AuthenticationAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
