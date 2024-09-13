4af9c044f28382b628b47ff32591b16380cd732e
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-09-13 19:14:38 +0200

    Basic Auth API

---
+++ Bangla.Services.AuthenticationAPI/Controllers/AuthenticationAPIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bangla.Services.AuthenticationAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationAPIController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Registration()
+        {
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
