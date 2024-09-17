39f657983c72003d41ab6a38ee156d652b22a0c4
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-09-17 10:11:29 +0200

    Registration Completion with frontend

--- Future.Bangla.Web/Controllers/AuthController.cs
+++ Future.Bangla.Web/Controllers/AuthController.cs
@@ -13,7 +13,7 @@ namespace Future.Bangla.Web.Controllers
         {
             _authService = authService;
         }
-        [HttpPost]
+        [HttpGet]
         public async Task<IActionResult> Login(LoginRequestDto loginRequest)
         {
 
@@ -22,14 +22,58 @@ namespace Future.Bangla.Web.Controllers
            return View(loginRequestDto);
         }
 
-        [HttpPost]
+        [HttpGet]
         public async Task<IActionResult> Register()
         {
+
             var roleList = new List<SelectListItem>()
             {
                 new SelectListItem { Text = SD.RoleAdmin, Value = SD.RoleAdmin },
                 new SelectListItem { Text = SD.RoleCustomer, Value = SD.RoleCustomer }
             };
+
+            // Viewbag for dropdown list for roles
+            ViewBag.RoleList = roleList;
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            ResponseDto? responseDto = await _authService.RegisterAsync(registrationRequestDto);
+
+            ResponseDto? assignRole;
+
+            if(responseDto != null && responseDto.IsSuccess) 
+            {
+                // By Default user is assigned to cutomer role
+                if(string.IsNullOrEmpty(registrationRequestDto.Role))
+                {
+                    registrationRequestDto.Role = SD.RoleCustomer;
+                }
+
+                // Role is assigned if we want to set role manually
+                assignRole =  await _authService.AssignToRoleAsync(registrationRequestDto);
+                
+                if(assignRole != null && responseDto.IsSuccess)
+                {
+                    TempData["success"] = "Registration Successful!";
+                    return RedirectToAction(nameof(Login));
+                }
+            
+            }
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem { Text = SD.RoleAdmin, Value = SD.RoleAdmin },
+                new SelectListItem { Text = SD.RoleCustomer, Value = SD.RoleCustomer }
+            };
+
+            // Viewbag for dropdown list for roles
+            ViewBag.RoleList = roleList;
 
             return View();
         }
