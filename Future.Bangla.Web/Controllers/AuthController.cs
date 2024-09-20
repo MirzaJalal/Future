705e321f8cc1db2dfc6a181d2a1f96af24b46b67
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-09-20 13:16:01 +0200

    Logout MVC in Action

--- Future.Bangla.Web/Controllers/AuthController.cs
+++ Future.Bangla.Web/Controllers/AuthController.cs
@@ -1,18 +1,25 @@
 ﻿using Future.Bangla.Web.Models;
 using Future.Bangla.Web.Service.IService;
 using Future.Bangla.Web.Utility;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Future.Bangla.Web.Controllers
 {
     public class AuthController : Controller
     {
         private readonly IAuthService _authService;
-        public AuthController(IAuthService authService)
+        private readonly ITokenProvider _tokenProvider;
+        public AuthController(IAuthService authService, ITokenProvider tokenProvider)
         {
             _authService = authService;
+            _tokenProvider = tokenProvider;
+
         }
         [HttpGet]
         public async Task<IActionResult> Login()
@@ -30,7 +37,10 @@ namespace Future.Bangla.Web.Controllers
 
             if (responseDto != null && responseDto.IsSuccess)
             {
-                LoginResponseDto? loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+                LoginResponseDto? loginResponseDto =  JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+
+                await SignInUser(loginResponseDto);
+                _tokenProvider.SetToken(loginResponseDto.Token);
 
                 return RedirectToAction("Index", "Home");
             }
@@ -99,8 +109,39 @@ namespace Future.Bangla.Web.Controllers
 
         public async Task<IActionResult> Logout()
         {
+            await HttpContext.SignOutAsync();
+            _tokenProvider.clearToken();
 
-            return View();
+            return RedirectToAction("Index","Home");
+        }
+
+        // <Summary>
+        //   Default steps to sign in a user to login using .NET Identity
+        // </Summary>
+        private async Task SignInUser(LoginResponseDto loginResponseDto)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            JwtSecurityToken jwt = handler.ReadJwtToken(loginResponseDto.Token);
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+
+
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Email, 
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Sub,
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Sub).Value));
+
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Name,
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Name).Value));
+
+
+            identity.AddClaim(new Claim(ClaimTypes.Name,
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
         }
     }
 }
--- Future.Bangla.Web/Utility/SD.cs
+++ Future.Bangla.Web/Utility/SD.cs
@@ -7,7 +7,7 @@
 
         public const string RoleAdmin = "Admin";
         public const string RoleCustomer = "Customer";
-        public const string TokenCookie = "JwtToken";
+        public const string TokenCookie = "JwtTokenClaimVerification";
         public enum ApiType
         {
             GET,
