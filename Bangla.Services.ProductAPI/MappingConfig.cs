44acca62b249dfbd9fa12ecf30783c75b7fecdf0
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-09-22 13:29:22 +0200

    mappings, conntectionString+Configurations+Program.cs, migration

---
+++ Bangla.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Bangla.Services.ProductAPI.Models;
+using Bangla.Services.ProductAPI.Models.Dto;
+
+namespace Bangla.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
