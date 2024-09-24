5414f51273c2720e690604ba3f86197c39776da0
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-09-24 19:21:46 +0200

    DbContext, mappings and config

---
+++ Builder.Services.ShoppingCartAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Builder.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bangla.Services.ShoppingCartAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        }
+        public DbSet<ShoppingCartDetails>? CartDetails { get; set; } // Plural coupons will be the table name
+        public DbSet<ShoppingCartHeader>? CartHeaders { get; set; } // Plural coupons will be the table name
+    }
+}
--- Builder.Services.ShoppingCartAPI/Program.cs
+++ Builder.Services.ShoppingCartAPI/Program.cs
@@ -1,11 +1,63 @@
+using AutoMapper;
+using Bangla.Services.ShoppingCartAPI;
+using Bangla.Services.ShoppingCartAPI.Data;
+using Bangla.Services.ShoppingCartAPI.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDatabaseConnection"));
+});
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper); // available throughout the lifecycle
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); // to use in the DI
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    // Enable the JWT bearer token input in Swagger UI
+    c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+
+                    //JwtBearerDefaults.AuthenticationScheme is same as "Bearer"
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+// ### STEP-1,2: Adding Authentication services ###
+builder.AddAppAuthentication();
+// ### STEP-3: Adding Authorization services ###
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -17,9 +69,24 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        ApplicationDbContext _db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
