132bebaaa46b1cb33dd048dd7a871f482f297cff
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-09-24 18:13:33 +0200

    adding models and Dtos

---
+++ Builder.Services.ShoppingCartAPI/Models/Dto/ShoppingCartDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Builder.Services.ShoppingCartAPI.Models.Dto
+{
+    public class ShoppingCartDetailsDto
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public ShoppingCartHeader? ShoppingCartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Builder.Services.ShoppingCartAPI/Models/ShoppingCartDetails.cs
@@ -0,0 +1,17 @@
+﻿using Builder.Services.ShoppingCartAPI.Models.Dto;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Builder.Services.ShoppingCartAPI.Models
+{
+    public class ShoppingCartDetails
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        [ForeignKey(nameof(CartHeaderId))]
+        public ShoppingCartHeader? ShoppingCartHeader { get; set; }
+        public int ProductId { get; set; }
+        [NotMapped]
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
