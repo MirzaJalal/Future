9e6a9adf629646160a18a6cbe066a1be8ec9856d
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-09-25 11:31:05 +0200

    migrations

--- Builder.Services.ShoppingCartAPI/Models/ShoppingCartDetails.cs
+++ Builder.Services.ShoppingCartAPI/Models/ShoppingCartDetails.cs
@@ -1,10 +1,12 @@
 ﻿using Builder.Services.ShoppingCartAPI.Models.Dto;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Builder.Services.ShoppingCartAPI.Models
 {
     public class ShoppingCartDetails
     {
+        [Key]
         public int CartDetailsId { get; set; }
         public int CartHeaderId { get; set; }
         [ForeignKey(nameof(CartHeaderId))]
