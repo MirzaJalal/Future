09f04a72b9ced26809d6aed373346523d2e9f56f
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-10-19 21:39:03 +0200

    Configurations and database migration

---
+++ Bangla.Services.OrderAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bangla.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bangla.Services.OrderAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        }
+        public DbSet<OrderDetails>? OrderDetails { get; set; } // Plural coupons will be the table name
+        public DbSet<OrderHeader>? OrderHeaders { get; set; } // Plural coupons will be the table name
+    }
+}
---
+++ Bangla.Services.OrderAPI/Utility/TokenPropagationHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+
+namespace Builder.Services.OrderAPI.Utility
+{
+    public class TokenPropagationHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TokenPropagationHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // Get the token from the current HTTP context in Service A
+            await _httpContextAccessor.HttpContext.GetTokenAsync("access_token");
+            var token = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            // Attach the token to the outgoing request to Service B
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            // Call the next handler in the pipeline (or send the request)
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+
+}
