61ea0e62cb5ab9090335387bb984040f12258307
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-10-22 21:08:57 +0200

    StripeRequestUrl dto for WebApp and OrderAPI

---
+++ Bangla.Services.OrderAPI/Models/Dtos/StripeRequestUrl.cs
@@ -0,0 +1,11 @@
+﻿namespace Bangla.Services.OrderAPI.Models.Dtos
+{
+    public class StripeRequestUrl
+    {
+        public string StipeSessionId { get; set; }
+        public string StripeSesstionUrl { get; set; }
+        public string ApprovedUrl { get; set; }
+        public string CalcelUrl { get; set; }
+        public OrderHeaderDto OrderHeaderDto { get; set; }
+    }
+}
---
+++ Future.Bangla.Web/Models/StripeRequestUrl.cs
@@ -0,0 +1,11 @@
+﻿namespace Future.Bangla.Web.Models
+{
+    public class StripeRequestUrl
+    {
+        public string StipeSessionId { get; set; }
+        public string StripeSesstionUrl { get; set; }
+        public string ApprovedUrl { get; set; }
+        public string CalcelUrl { get; set; }
+        public OrderHeaderDto OrderHeaderDto { get; set; }
+    }
+}
