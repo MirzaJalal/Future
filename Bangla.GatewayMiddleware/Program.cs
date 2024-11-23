db3ee56f37c12256491203af308f957ce299417c
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-11-24 00:24:34 +0100

    Clean coding for Ocelot.json file

--- Bangla.GatewayMiddleware/Program.cs
+++ Bangla.GatewayMiddleware/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthentication();
 
 builder.Services.AddOcelot();
+// Configure Ocelot to load configuration from ocelot.json
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 var app = builder.Build();
 
