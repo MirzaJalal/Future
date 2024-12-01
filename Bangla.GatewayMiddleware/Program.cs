fdca8a229c985f5c072f5d0ba1a6aea729f98b2e
Mirza Jalal Uddin <mirzajalal25@gmail.com>
2024-12-01 19:17:57 +0100

    Ocelot Gateway deployment

--- Bangla.GatewayMiddleware/Program.cs
+++ Bangla.GatewayMiddleware/Program.cs
@@ -5,13 +5,25 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthentication();
 
-builder.Services.AddOcelot();
+if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
+{
+    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
+}
+else
+{
+    builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+}
+
+
+builder.Services.AddOcelot(builder.Configuration);
 // Configure Ocelot to load configuration from ocelot.json
-builder.Configuration
-    .SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+//builder.Configuration
+//    .SetBasePath(builder.Environment.ContentRootPath)
+//    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 await app.UseOcelot();
--- Bangla.Services.ProductAPI/Data/ApplicationDbContext.cs
+++ Bangla.Services.ProductAPI/Data/ApplicationDbContext.cs
@@ -17,38 +17,38 @@ namespace Bangla.Services.ProductAPI.Data
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 ProductId = 1,
-                Name = "Samosa",
+                Name = "Biriyani",
                 Price = 15,
-                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                Description = "Lorem ipsum dolor sit amet. Et voluptatum laudantium eos dolorem ratione est doloribus nesciunt ea architecto unde ut consequuntur neque. Et neque illum aut vero ipsam sed quibusdam voluptatibus qui internos exercitationem qui magni animi qui fuga neque quo commodi consequatur. In repudiandae deserunt eos excepturi quibusdam et rerum voluptatem vel magni officia rem voluptatem expedita qui praesentium quod ea inventore corporis. Qui assumenda dignissimos aut voluptas veritatis ut ipsa velit est soluta quaerat in adipisci quis. </p><p>Ut consequuntur error et dolorum aperiam et velit amet aut laborum optio aut voluptas dicta cum voluptatem consequuntur. Aut rerum rerum sed iure perferendis rem optio earum et voluptate totam. </p><p>Ut dicta omnis et laboriosam repellat eos assumenda dolore est dolor temporibus aut porro eligendi qui error molestiae! At sint natus quo omnis mollitia et optio quisquam et natus nihil qui consequatur maiores. Hic repellendus consequatur et consequatur nisi qui adipisci voluptatem. Et nisi molestiae aut internos tenetur quo similique odio?",
                 ImageUrl = "https://placehold.co/603x403",
-                CategoryName = "Appetizer"
+                CategoryName = "Main Dish"
             });
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 ProductId = 2,
-                Name = "Paneer Tikka",
+                Name = "Fuchka",
                 Price = 13.99,
-                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                Description = "Lorem ipsum dolor sit amet. Et voluptatum laudantium eos dolorem ratione est doloribus nesciunt ea architecto unde ut consequuntur neque. Et neque illum aut vero ipsam sed quibusdam voluptatibus qui internos exercitationem qui magni animi qui fuga neque quo commodi consequatur. In repudiandae deserunt eos excepturi quibusdam et rerum voluptatem vel magni officia rem voluptatem expedita qui praesentium quod ea inventore corporis. Qui assumenda dignissimos aut voluptas veritatis ut ipsa velit est soluta quaerat in adipisci quis. </p><p>Ut consequuntur error et dolorum aperiam et velit amet aut laborum optio aut voluptas dicta cum voluptatem consequuntur. Aut rerum rerum sed iure perferendis rem optio earum et voluptate totam. </p><p>Ut dicta omnis et laboriosam repellat eos assumenda dolore est dolor temporibus aut porro eligendi qui error molestiae! At sint natus quo omnis mollitia et optio quisquam et natus nihil qui consequatur maiores. Hic repellendus consequatur et consequatur nisi qui adipisci voluptatem. Et nisi molestiae aut internos tenetur quo similique odio?",
                 ImageUrl = "https://placehold.co/602x402",
-                CategoryName = "Appetizer"
+                CategoryName = "Snacks"
             });
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 ProductId = 3,
-                Name = "Sweet Pie",
+                Name = "Kala Vuna",
                 Price = 10.99,
-                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                Description = "Lorem ipsum dolor sit amet. Et voluptatum laudantium eos dolorem ratione est doloribus nesciunt ea architecto unde ut consequuntur neque. Et neque illum aut vero ipsam sed quibusdam voluptatibus qui internos exercitationem qui magni animi qui fuga neque quo commodi consequatur. In repudiandae deserunt eos excepturi quibusdam et rerum voluptatem vel magni officia rem voluptatem expedita qui praesentium quod ea inventore corporis. Qui assumenda dignissimos aut voluptas veritatis ut ipsa velit est soluta quaerat in adipisci quis. </p><p>Ut consequuntur error et dolorum aperiam et velit amet aut laborum optio aut voluptas dicta cum voluptatem consequuntur. Aut rerum rerum sed iure perferendis rem optio earum et voluptate totam. </p><p>Ut dicta omnis et laboriosam repellat eos assumenda dolore est dolor temporibus aut porro eligendi qui error molestiae! At sint natus quo omnis mollitia et optio quisquam et natus nihil qui consequatur maiores. Hic repellendus consequatur et consequatur nisi qui adipisci voluptatem. Et nisi molestiae aut internos tenetur quo similique odio?",
                 ImageUrl = "https://placehold.co/601x401",
-                CategoryName = "Dessert"
+                CategoryName = "Traditional"
             });
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 ProductId = 4,
-                Name = "Pav Bhaji",
+                Name = "Vapa Pitha",
                 Price = 15,
-                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                Description = "Lorem ipsum dolor sit amet. Et voluptatum laudantium eos dolorem ratione est doloribus nesciunt ea architecto unde ut consequuntur neque. Et neque illum aut vero ipsam sed quibusdam voluptatibus qui internos exercitationem qui magni animi qui fuga neque quo commodi consequatur. In repudiandae deserunt eos excepturi quibusdam et rerum voluptatem vel magni officia rem voluptatem expedita qui praesentium quod ea inventore corporis. Qui assumenda dignissimos aut voluptas veritatis ut ipsa velit est soluta quaerat in adipisci quis. </p><p>Ut consequuntur error et dolorum aperiam et velit amet aut laborum optio aut voluptas dicta cum voluptatem consequuntur. Aut rerum rerum sed iure perferendis rem optio earum et voluptate totam. </p><p>Ut dicta omnis et laboriosam repellat eos assumenda dolore est dolor temporibus aut porro eligendi qui error molestiae! At sint natus quo omnis mollitia et optio quisquam et natus nihil qui consequatur maiores. Hic repellendus consequatur et consequatur nisi qui adipisci voluptatem. Et nisi molestiae aut internos tenetur quo similique odio?",
                 ImageUrl = "https://placehold.co/600x400",
-                CategoryName = "Entree"
+                CategoryName = "Winter Dish"
             });
 
         }
